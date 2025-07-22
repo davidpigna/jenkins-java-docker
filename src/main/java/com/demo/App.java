4f62afbe38e32950e86d103c21126103ba8122c7
davidpigna <67974814+davidpigna@users.noreply.github.com>
2025-07-21 22:31:39 -0300

    Pom and app updated

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,11 +1,10 @@
 package com.demo;
 
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Java Demo!");
-    }
-
-    public static int suma(int a, int b) {
-        return a + b;
+        port(8080);
+        get("/", (req, res) -> "Hello from Jenkins Java Demo!");
     }
 }
